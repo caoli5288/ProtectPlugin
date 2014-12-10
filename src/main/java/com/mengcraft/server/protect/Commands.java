79b08021779fc610abca0f7dd61e24630ae0b44e
yiwan <caoli5288@gmail.com>
2014-12-11 03:08:22 +0800

    new command provide unload loaded chunks

--- src/main/java/com/mengcraft/server/protect/Commands.java
+++ src/main/java/com/mengcraft/server/protect/Commands.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,27 +17,71 @@ import org.bukkit.entity.EntityType;
 
 public class Commands implements CommandExecutor {
 
+	private String[] getPluginInfo() {
+		String[] strings = new String[] {
+				ChatColor.GOLD + "/protect info entity",
+				ChatColor.GOLD + "/protect info chunk",
+				ChatColor.GOLD + "/protect purge entity [ENTITY_TYPE]",
+				ChatColor.GOLD + "/protect purge chunk"
+		};
+		return strings;
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
 		if (args.length < 1) {
 			sender.sendMessage(getPluginInfo());
 		} else if (args.length < 2) {
-
+			if (args[0].equals("unchunk")) {
+				sender.sendMessage(purgeChunk());
+			}
 		} else if (args.length < 3) {
 			if (args[0].equals("info")) {
 				if (args[1].equals("entity")) {
 					sender.sendMessage(getEntityInfo());
+				} else if (args[1].equals("chunk")) {
+					sender.sendMessage(getChunkInfo());
 				}
 			} else if (args[0].equals("purge")) {
-				sender.sendMessage(purgeEntity(args[1]));
+				if (args[1].equals("chunk")) {
+					sender.sendMessage(purgeChunk());
+				}
+			}
+		} else if (args.length < 4) {
+			if (args[0].equals("purge")) {
+				if (args[1].equals("entity")) {
+					sender.sendMessage(purgeEntity(args[2]));
+				}
 			}
 		}
 		return true;
 	}
 
+	private String[] getChunkInfo() {
+		List<String> messages = new ArrayList<>();
+		int total = 0;
+		for (World world : Bukkit.getWorlds()) {
+			int size = world.getLoadedChunks().length;
+			messages.add(ChatColor.GOLD + world.getName() + ": " + size);
+		}
+		messages.add(ChatColor.GOLD + "Total: " + total);
+		int size = messages.size();
+		return messages.toArray(new String[size]);
+	}
+
+	private String purgeChunk() {
+		int value = 0;
+		for (World world : Bukkit.getWorlds()) {
+			for (Chunk chunk : world.getLoadedChunks()) {
+				value = chunk.unload(true, true) ? value + 1 : value;
+			}
+		}
+		return new String(ChatColor.GOLD + "Purge chunk number: " + value);
+	}
+
 	private String purgeEntity(String typeName) {
 		if (typeName.equals("PLAYER")) {
-			return new String(ChatColor.GOLD + "You can not purge player entity"); 
+			return new String(ChatColor.GOLD + "You can not purge player entity");
 		}
 		int total = 0;
 		for (World world : Bukkit.getWorlds()) {
@@ -80,14 +125,6 @@ public class Commands implements CommandExecutor {
 		messages.add(ChatColor.GOLD + "TOTAL: " + total);
 		int size = messages.size();
 		return messages.toArray(new String[size]);
-	}
-
-	private String[] getPluginInfo() {
-		String[] strings = new String[] {
-				ChatColor.GOLD + "/protect info entity",
-				ChatColor.GOLD + "/protect purge [ENTITY_TYPE]"
-		};
-		return strings;
 	}
 
 }
