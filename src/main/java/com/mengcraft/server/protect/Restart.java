0ba7a3dbee1cf0737a9c582e6d3c15f674762fec
yiwan <caoli5288@gmail.com>
2014-12-15 21:42:27 +0800

    Revert "add option restart online player limit"
    This reverts commit 879723e0c4bfed0af44abf70391d2558e3a6437e.

--- src/main/java/com/mengcraft/server/protect/Restart.java
+++ src/main/java/com/mengcraft/server/protect/Restart.java
@@ -3,16 +3,9 @@ package com.mengcraft.server.protect;
 import org.bukkit.Bukkit;
 
 public class Restart implements Runnable {
-
-	private final int limit;
-
-	public Restart(int limit) {
-		this.limit = limit;
-	}
-
 	@Override
 	public void run() {
-		if (Bukkit.getOnlinePlayers().length <= this.limit) {
+		if (Bukkit.getOnlinePlayers().length < 1) {
 			Bukkit.shutdown();
 		}
 	}
