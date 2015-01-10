2d924782a2aad7e904b803e38b25792e5dc02b15
yiwan <caoli5288@gmail.com>
2015-01-10 17:59:22 +0800

    anti overload compatible with 1.8

--- src/main/java/com/mengcraft/bukkit/protect/anti/AntiOverload.java
+++ src/main/java/com/mengcraft/bukkit/protect/anti/AntiOverload.java
@@ -1,5 +1,7 @@
 package com.mengcraft.bukkit.protect.anti;
 
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
@@ -12,11 +14,59 @@ import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class AntiOverload implements Listener {
 
+	private final List<?> handle;
+
+	public AntiOverload() {
+		Object playerList = getPlayerListField();
+		handle = getHandle(playerList);
+	}
+
+	private List<?> getHandle(Object playerList) {
+		Class<?> c = playerList.getClass().getSuperclass();
+		Field field = getField(c, "players");
+		Object object = null;
+		try {
+			object = field.get(playerList);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return (List<?>) object;
+	}
+
+	private Object getPlayerListField() {
+		Class<?> server = Bukkit.getServer().getClass();
+		Field field = getField(server, "playerList");
+		Object object = null;
+		try {
+			object = field.get(Bukkit.getServer());
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return object;
+	}
+
+	private Field getField(Class<?> server, String string) {
+		Field field = null;
+		try {
+			field = server.getDeclaredField(string);
+			field.setAccessible(true);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		return field;
+	}
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onLogin(PlayerLoginEvent event) {
 		if (event.getResult().equals(Result.ALLOWED)) {
 			int max = Bukkit.getMaxPlayers();
-			while (Bukkit.getOnlinePlayers().length > max) {
+			while (this.handle.size() > max) {
 				randomKick(Bukkit.getOnlinePlayers());
 			}
 		}
