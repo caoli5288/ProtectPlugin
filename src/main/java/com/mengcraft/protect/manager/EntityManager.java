75ac277aab52f028236b29ea6b62838e99687b1c
yiwan <caoli5288@gmail.com>
2015-01-24 18:53:36 +0800

    Change EntityManager. compatible with other.

--- src/main/java/com/mengcraft/protect/manager/EntityManager.java
+++ src/main/java/com/mengcraft/protect/manager/EntityManager.java
@@ -113,6 +113,9 @@ public class EntityManager {
 		public void onCreatureSpawn(CreatureSpawnEvent event) {
 			int i = check(event.getEntity(), this.limit);
 			event.setCancelled(i > 0);
+			if (i > 0) {
+				event.setCancelled(true);
+			}
 		}
 
 	}
