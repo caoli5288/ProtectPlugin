05f956ad3629936bbc56fc554a891ed13066af7a
yiwan <caoli5288@gmail.com>
2015-02-23 21:59:03 +0800

    improve anti copy item by break a in used chest

--- src/main/java/com/mengcraft/protect/listener/AntiCopyItem.java
+++ src/main/java/com/mengcraft/protect/listener/AntiCopyItem.java
@@ -2,19 +2,19 @@ package com.mengcraft.protect.listener;
 
 import org.bukkit.ChatColor;
 import org.bukkit.block.BlockState;
-import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 public class AntiCopyItem implements Listener {
 
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
 	public void handle(BlockBreakEvent event) {
 		BlockState state = event.getBlock().getState();
-		if (state instanceof Chest) {
-			Chest chest = (Chest) state;
+		if (state instanceof InventoryHolder) {
+			InventoryHolder chest = (InventoryHolder) state;
 			if (check(chest) > 0) {
 				event.setCancelled(true);
 				event.getPlayer().sendMessage(ChatColor.RED + "你无法破坏正在被使用的箱子");
@@ -22,8 +22,8 @@ public class AntiCopyItem implements Listener {
 		}
 	}
 
-	private int check(Chest state) {
-		return state.getBlockInventory().getViewers().size();
+	private int check(InventoryHolder state) {
+		return state.getInventory().getViewers().size();
 	}
 
 }
